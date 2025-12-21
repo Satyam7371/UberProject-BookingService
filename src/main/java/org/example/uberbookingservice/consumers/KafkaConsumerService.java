ec83d04e0a96ef1af5b4942509401c5d436a4a22
Satyam Choubey <satyam7371@gmail.com>
2025-12-21 22:16:56 +0530

    added kafka configuration

---
+++ src/main/java/org/example/uberbookingservice/consumers/KafkaConsumerService.java
@@ -0,0 +1,13 @@
+package org.example.uberbookingservice.consumers;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "sample-topic")
+    public void listen(String message) {
+        System.out.println("Message from topic sample-topic inside booking service: " + message);
+    }
+}
